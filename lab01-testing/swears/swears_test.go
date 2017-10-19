1df163b61e2ef7d1924820dbdfedb9000152cad1
Royce Le <roycele@D-108-179-131-65.dhcp4.washington.edu>
2017-10-19 15:35:12 -0700

    lab swears test cases

--- lab01-testing/swears/swears_test.go
+++ lab01-testing/swears/swears_test.go
@@ -3,11 +3,63 @@ package main
 import "testing"
 
 func TestcountSwears(t *testing.T) {
+	/*cases := []struct {
+		name           string
+		input1          *KnownSwears
+		input2			*[]string
+		expectedOutput *SwearCounts
+	}{
+		{
+			name:           "empty file",
+			input:          [{"fuck", false}, {"shit"}],
+			expectedOutput: 0,
+		},
+		{
+			name:           "single character",
+			input:          "a",
+			expectedOutput: "a",
+		},
+		{
+			name:           "two characters",
+			input:          "ab",
+			expectedOutput: "ba",
+		},
+		{
+			name:           "stressed",
+			input:          "stressed",
+			expectedOutput: "desserts",
+		},
+		{
+			name:           "high unicode",
+			input:          "Hello, 世界",
+			expectedOutput: "界世 ,olleH",
+		},
+	}
 
+	for _, c := range cases {
+		if output := Reverse(c.input); output != c.expectedOutput {
+			t.Errorf("%s: got %s but expected %s", c.name, output, c.expectedOutput)
+		}
+	}*/
 }
 
 func TestloadKnownSwears(t *testing.T) {
-
+	cases := []struct {
+		name           string
+		input          string
+		expectedOutput *KnownSwears
+	}{
+		{
+			name:           "empty file",
+			input:          "",
+			expectedOutput: &map[string]bool,
+		},
+	}
+	for _, c := range cases {
+		if output := Reverse(c.input); output != c.expectedOutput {
+			t.Errorf("%s: got %s but expected %s", c.name, output, c.expectedOutput)
+		}
+	}
 }
 
 func TestloadWords(t *testing.T) {
--- lab01-testing/test_handlers/handlers_test.go
+++ lab01-testing/test_handlers/handlers_test.go
@@ -1,7 +1,40 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
 
 func TestComplimentHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/compliments", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	name := "steve"
+	q := req.URL.Query()
+	q.Add("name", name)
+	req.URL.RawQuery = q.Encode()
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ComplimentHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	//What is %d vs %v?
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler return the wrong status code: got %d want %d",
+			status, http.StatusOK)
+	}
+
+	expectedPrefix := fmt.Sprintf("%s, you are", name)
+
+	if !strings.HasPrefix(rr.Body.String(), expectedPrefix) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expectedPrefix)
+	}
 
 }
